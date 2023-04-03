9fb77c6823d099c0758c03dbc54539aaab9b2839
brokenes <xie_xieyu@sina.com>
2023-04-03 13:30:30 +0800

    init

--- admin-api/src/main/java/com/github/admin/api/resolver/MyLocaleResolver.java
+++ admin-api/src/main/java/com/github/admin/api/resolver/MyLocaleResolver.java
@@ -1,21 +1,15 @@
 package com.github.admin.api.resolver;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.LocaleResolver;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Locale;
 
 //@Service
 public class MyLocaleResolver implements LocaleResolver {
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(MyLocaleResolver.class);
-
-
-
 	@Override
 	public Locale resolveLocale(HttpServletRequest request) {
 		String lan = request.getParameter("lan");
@@ -26,11 +20,9 @@ public class MyLocaleResolver implements LocaleResolver {
         String[] ii = lan.split("_");
         return new Locale(ii[0], ii[1]);
 	}
-
 	@Override
 	public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
 
 	}
-
 
 }
