5eec868dbfc8346c036c4b934c69e61c6abe5273
brokenes <xie_xieyu@sina.com>
2023-04-06 16:49:54 +0800

    init

--- admin-api/src/main/java/com/github/admin/api/controller/MainController.java
+++ admin-api/src/main/java/com/github/admin/api/controller/MainController.java
@@ -1,30 +1,37 @@
 package com.github.admin.api.controller;
 
 import com.github.admin.client.MenuServiceClient;
+import com.github.admin.client.RoleServiceClient;
 import com.github.admin.common.constants.AdminConst;
 import com.github.admin.common.domain.Menu;
+import com.github.admin.common.domain.Role;
 import com.github.admin.common.domain.User;
 import com.github.admin.common.enums.MenuTypeEnum;
+import com.github.admin.common.enums.StatusEnum;
 import com.github.admin.common.util.Result;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class MainController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
-    @Resource
+    @Autowired
     private MenuServiceClient menuServiceClient;
+
+    @Autowired
+    private RoleServiceClient roleServiceClient;
 
     @GetMapping("/main")
     @RequiresPermissions("index")
@@ -43,6 +50,18 @@ public class MainController {
                 menus.forEach(menu -> keyMenu.put(menu.getId(), menu));
             }else{
                 //TODO
+                // 其他用户需从相应的角色中获取菜单资源
+                Result<Set<Role>> roleResult = roleServiceClient.getUserOkRoleList(user.getId());
+                if(roleResult.isSuccess()){
+                    Set<Role> roles = roleResult.getData();
+                    roles.forEach(role -> {
+                        role.getMenus().forEach(menu -> {
+                            if(menu.getStatus() == StatusEnum.OK.getCode()){
+                                keyMenu.put(menu.getId(), menu);
+                            }
+                        });
+                    });
+                }
             }
             keyMenu.forEach((id, menu) -> {
                 if(menu.getType() != MenuTypeEnum.BUTTON.getCode()){
